80e5270f2eedb54617ceaab4ea8caf5aa63796a1
Javier Prada <javiprada1@gmail.com>
2020-01-22 20:58:13 +0100

    Add Frontend

---
+++ frontend/src/app/models/teacher.ts
@@ -0,0 +1,16 @@
+export class Teacher {
+
+    _id: String;
+    name: String;
+    surname: String;
+    area: String;
+    salary: Number;
+
+    constructor(_id='', name='', surname='', area='', salary=0){
+        this._id=_id;
+        this.name=name;
+        this.surname=surname;
+        this.area=area;
+        this.salary=salary;
+    }
+}
